e564e6cd04f907003247c0184038bf7d979f7dcf
felix <felixsun6688@gmail.com>
2018-09-04 12:33:51 +0800

    Create folders

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dn
+ * Date: 2018/9/4
+ * Time: 12:22
+ */
+
+require "smartphp/core/Smartphp.php";
+
+Smartphp::run();
---
+++ smartphp/core/Smartphp.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dn
+ * Date: 2018/9/4
+ * Time: 12:31
+ */
+
+class Smartphp{
+
+    public  static function run(){
+        echo "run()";
+    }
+}
